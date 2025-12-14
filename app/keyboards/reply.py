892311a798d378083fc026adff0b0b01afecf58b
aider-1 <losadkaslal@gmail.com>
2025-12-14 22:46:54 +0500

    удалил reply

--- app/keyboards/reply.py
+++
@@ -1,5 +0,0 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-
-start_menu = ReplyKeyboardMarkup(keyboard=[
-    [KeyboardButton(text="Добавить дату"), KeyboardButton(text="Список дат")]
-], resize_keyboard=True, one_time_keyboard=True)
